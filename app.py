16f9ca4ce9b8a2053f055354fd32c1e770782a44
sheng-kai-wang <76858274+sheng-kai-wang@users.noreply.github.com>
2022-02-15 06:52:51 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def introduction():
+    return "This is a service for generating RASA training sentences, based on wordnet and spaCy.<br>You can use it with /generateSentences"
+
+@app.route('/generateSentences', methods=['POST'])
+def generateSentences():
+    return "request.data"
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', debug=True)
